b13e433ad6bbd608ab15e9c1e29fe8502fdc508e
prajsoft <prajsoft@gmail.com>
2022-08-31 23:41:01 +0530

    new

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+import styles from '../styles/Home.module.css';
+
+export default function Layout() {
+    
+
+return(
+<>
+<div className= 'styles.title'>
+<h1>Layout</h1>
+</div>
+
+</>
+
+)
+}
--- pages/careers.js
+++ pages/careers.js
@@ -1,9 +1,17 @@
 import NavBar from '/components/NavBar';
 import Footers from '../components/Footers';
+import Layout from '../components/Layout';
 export default function Careers() {
     return (
       <>
        < NavBar />
+       <Layout />
+       <Layout />
+       <Layout />
+       <Layout />
+       <Layout />
+       <Layout />
+       <Layout />
       <h1>Careers</h1>
       < Footers />
       </>
