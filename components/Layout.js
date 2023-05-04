92b3740e01b5d2e34f3bb00de08d29dda452baf1
prajsoft <prajsoft@gmail.com>
2023-05-05 00:21:49 +0530

    yes

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,19 @@
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
+import Blog from "./Blog";
 // import Link from 'next/link';
 
 export default function Layout() {
-    
-
-return(
-<>
-
-<h1>Layout Test</h1>
-<div className= {styles.gridLayoutContainer}>
-   <div>
-        <h1>Latest Articles</h1>
-        
-        
-     </div>
-   <div>
-        <h1>Articles</h1>
-   </div>
-</div>
-
-
-</>
-
-)
+  return (
+    <>
+      <h1>Layout Test</h1>
+      <div className={styles.gridLayoutContainer}>
+        <div>
+          <h1>Latest Articles</h1>
+        </div>
+        <div>
+          <h1>Articles</h1>
+        </div>
+      </div>
+    </>
+  );
 }
--- pages/degrees.js
+++ pages/degrees.js
@@ -1,22 +1,18 @@
-import NavBar from '/components/NavBar';
-import Footers from '../components/Footers';
-import Layout from '../components/Layout';
-import styles from '../styles/Home.module.css';
+import NavBar from "/components/NavBar";
+import Footers from "../components/Footers";
+import Layout from "../components/Layout";
+import styles from "../styles/Home.module.css";
+import Blog from "../components/Blog";
 
 export default function Degrees() {
-    return (
-      <>
+  return (
+    <>
       <div className={styles.container}>
-      
         <div className={styles.main}>
-            <h1>Degrees</h1>
-            <Layout />
-            
+          <h1>Degrees</h1>
+          <Layout />
         </div>
-      
       </div>
-      </>
-     
-
-    ) 
-  }
+    </>
+  );
+}
