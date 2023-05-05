65a7f837ac0522b02ea739db5c4052b4ad952108
prajsoft <prajsoft@gmail.com>
2023-05-05 22:19:02 +0530

    Card

--- components/Card.js
+++ components/Card.js
@@ -1,8 +1,15 @@
-export default function Card({ image, title, description }) {
+import Image from "next/image";
+
+export default function Card({ imageSrc, title, description }) {
   return (
     <>
       <div className="styles.card">
-        <img src={image} alt={title} className="styles.cimage" />
+        <Image
+          src={imageSrc}
+          alt={title}
+          className="styles.cimage"
+          layout="fill"
+        />
         <h2 className="styles.ctitle"> {title} </h2>
         <p className="styles.cdescription"> {description}</p>
       </div>
