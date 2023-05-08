af60b2830b6ce1a053e57f2cfc390cce714f67fb
prajsoft <prajsoft@gmail.com>
2023-05-08 07:39:52 +0530

    New Day

--- pages/posts/[Slug].js
+++ pages/posts/[Slug].js
@@ -1,14 +1,18 @@
-import { loadPosts } from "../../lib/load-posts";
-import { getPostById } from "../../lib/load-posts";
+import {
+  loadPosts,
+  getPostById,
+  prepareForSerializatoin,
+} from "../../lib/load-posts";
 
-export default function Post({ post }) {
+export default function BlogPost({ post }) {
   console.log("Post function");
   console.log(post);
   return (
     <>
       <div>
-        <h1>{post.attributes.Title}</h1>
-        <p>{post.attributes.Description}</p>
+        <h1>{post.data.id}</h1>
+        {/* <h1>{post.data.attributes.Title}</h1>
+        <p>{post.data.attributes.Description}</p> */}
       </div>
     </>
   );
@@ -27,33 +31,21 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({ params }) {
+// export async function getStaticProps(params) {
+//   const post = await getPostById(params.Slug);
+//   console.log("post", post);
+//   return prepareForSerializatoin({
+//     props: {
+//       post,
+//     },
+//   });
+// }
+export async function getStaticProps(params) {
   const post = await getPostById(params.Slug);
+  console.log("post", post);
   return {
     props: {
-      post,
+      ...post,
     },
   };
 }
-
-// export async function getStaticProps({ params }) {
-//   try {
-//     const data = await getPostById(params.Slug);
-//     if (!data) {
-//       throw new Error("No data received");
-//     }
-//     const post = await data.json();
-//     return {
-//       props: {
-//         post,
-//       },
-//     };
-//   } catch (error) {
-//     console.error(error);
-//     return {
-//       props: {
-//         post: null,
-//       },
-//     };
-//   }
-// }
