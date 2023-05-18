6f2edd5b7fe4f1a5238ba8fc24943e2ded5011fe
prajsoft <prajsoft@gmail.com>
2023-05-18 12:55:14 +0530

    Env variable

--- components/BlogLayout.js
+++ components/BlogLayout.js
@@ -4,9 +4,11 @@ import { useEffect, useRef } from "react";
 import htmlReactParser from "html-react-parser";
 
 export default function BlogLayout({ post }) {
+  const ApiUrl = process.env.API_URL;
   const attributes = post.data[0].attributes;
   const image = attributes.HeroPic.data.attributes.url;
-  const imageSrc = `http://localhost:1337${image}`;
+  const imageSrc = `${ApiUrl}${image}`;
+  console.log(imageSrc);
   const parallaxRef = useRef(null);
   useEffect(() => {
     const handleScroll = () => {
@@ -37,7 +39,6 @@ export default function BlogLayout({ post }) {
       <div className={styles.contentContainer}>
         <h1 className={styles.title}>{attributes.Title}</h1>
         <p className={styles.description}>{attributes.Description}</p>
-        {/* <div className={styles.content}>{attributes.Content}</div> */}
         <div className={styles.content}>
           {htmlReactParser(attributes.Content)}
         </div>
